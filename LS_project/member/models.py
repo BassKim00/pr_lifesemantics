6b155d5a51796542ea3061ec596f8cb4d9fbd2bf
BassKim <MY-kim@lifesemantics.kr>
2018-12-28 10:31:56 +0900

    django test server
    completed

--- LS_project/member/models.py
+++ LS_project/member/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-class Member(models.Modes):
+# Create your models here.
+class Member(models.Model):
 	name=models.CharField(max_length=200)
 	mail=models.CharField(max_length=200)
 	age=models.IntegerField(default=0)
