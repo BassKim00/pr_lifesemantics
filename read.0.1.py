4194d188e42f03b3efd77c83710096495092c4e8
bright-jun <43338179+bright-jun@users.noreply.github.com>
2018-12-28 14:54:17 +0900

    Update read.0.1.py
    merged to class

--- read.0.1.py
+++ read.0.1.py
@@ -1,21 +1,6 @@
-###
 import json
 from pprint import pprint
 import numpy as np
-
-def dis_kilo(unit,value):
-    if unit=="KILOMETER" or unit=="km":
-        return value
-    else:
-        return value/1000
-
-def cal_kilo(unit,value):
-    if unit=="KILOCALORIE" or unit=="kcal":
-        return value
-    else:
-        return value/1000
-
-
 
 #json형식을 str로 인식후 json으로 변환
 
@@ -28,13 +13,28 @@ with open('C:/Users/LS-COM-00044/Desktop/jsondata/activity_simple.json','r') as 
 
 class Log(object):
 
+    #unit converter
+    
+    def dis_kilo(self,unit,value):
+        if unit=="KILOMETER" or unit=="km":
+            return value
+        else:
+            return value/1000
 
+    def cal_kilo(self,unit,value):
+        if unit=="KILOCALORIE" or unit=="kcal":
+            return value
+        else:
+            return value/1000
+        
+    #initial
+        
     def __init__(self,_id,name,steps,distance,calories,createAt,period):
         self._id = _id
         self.name = name
         self.steps = steps
-        self.distance = distance
-        self.calories = calories
+        self.distance = self.dis_kilo(distance['unit'],distance['value'])
+        self.calories = self.dis_kilo(calories['unit'],calories['value'])
         self.createAt = createAt
         self.period = period
 
@@ -50,19 +50,19 @@ class User(object):
         self.time = time
 
     def __repr__(self):
-        return self._id
+        return self.name
 
     def week(self):
-        return self._id #1주일 평균 어떻게 구하지
+        return self.name #1주일 평균 어떻게 구하지
 
     def month(self):
-        return self._id #1달 평균 어떻게 구하지
+        return self.name #1달 평균 어떻게 구하지
 
 #log 할당
 
 log=[]
 for d in data:
-    log.append(Log(d['_id'],d['recordkey'],d['steps'],dis_kilo(d['distance']['unit'],d['distance']['value']),cal_kilo(d['calories']['unit'],d['calories']['value']),d['createAt'],d['period']))
+    log.append(Log(d['_id'],d['recordkey'],d['steps'],d['distance'],d['calories'],d['createAt'],d['period']))
 
 print("log[0]")
 pprint(log[0].__dict__)
