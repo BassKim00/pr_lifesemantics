2fa7b4298f82d4f0713c09a6442c9b09098450e7
BassKim <MY-kim@lifesemantics.kr>
2019-01-15 16:04:12 +0900

    Update settings.py

--- LS_project/LS_project/settings.py
+++ LS_project/LS_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&o^^=tz(lk34($+!mb43bg1lt2py&0oez@xw)29%^5hbhklzxb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1:8000",
+    '.ap-northeast-2.compute.amazonaws.com',
+]
 
 
 # Application definition
